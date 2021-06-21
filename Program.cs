1f06a54eb7c4d1824db3d7d205cc7007e7cdfc46
Hassan Mohamed <hassanalimohamed1@gmail.com>
2021-06-21 11:35:51 +1000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+namespace MoiveTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMoveTime("Matrix", 113);
+            DisplayMoveTime("300");
+
+
+        }
+
+        static void DisplayMoveTime(string mov, int min = 90) {
+
+            WriteLine($"The moive is {mov} is {min} minutes long.");
+        }
+
+    }
+}
